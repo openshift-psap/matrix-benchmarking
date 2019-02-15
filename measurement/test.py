11f3499a1b368ab47622395c5e6392200a56cbd2
Frediano Ziglio <fziglio@redhat.com>
2019-02-15 17:01:45 +0000

    test: No need to keep the file open during the test
    The file is passed to the sub process.

--- measurement/test.py
+++ measurement/test.py
@@ -8,12 +8,11 @@ class Test(measurement.Measurement):
         try: os.unlink(self.log)
         except: pass
         # start vmstat
-        self.out = open(self.log, 'w')
-        self.vmstat = subprocess.Popen('vmstat -t 1'.split(), stdout=self.out, close_fds=True)
+        out = open(self.log, 'w')
+        self.vmstat = subprocess.Popen('vmstat -t 1'.split(), stdout=out, close_fds=True)
     def stop(self):
         self.vmstat.terminate()
         self.vmstat.wait()
-        del self.out
     def collect(self):
         # TODO parse log and save a table of something
         os.system("cat %s" % self.log)
