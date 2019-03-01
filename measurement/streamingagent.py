7c9b175c624a5cc4332e64940a8f14cbd1cb5d25
Frediano Ziglio <fziglio@redhat.com>
2019-03-01 14:28:54 +0000

    Implement parsed for streaming agent log
    TODO: fix the time, it's not saving to the database

---
+++ measurement/streamingagent.py
@@ -0,0 +1,55 @@
+import os
+import datetime
+import inspect
+import re
+import measurement
+
+class StreamingAgent(measurement.Measurement):
+    def __init__(self, cfg=None, **kargs):
+        super().__init__(**kargs)
+        # TODO final file
+        cur_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+        self.log = os.path.join(cur_dir, '..', 'streaming.log')
+        self.table = self.experiment.create_table([
+            'time',
+            'guest.frame_size',
+            'guest.capture_duration',
+            'guest.send_duration',
+        ])
+
+    def start(self):
+        # TODO
+        pass
+
+    def stop(self):
+        # TODO
+        pass
+
+    def collect(self):
+        # TODO retrieve
+        # parse log
+        line_re = re.compile(r'^(\d+): (\w+)(.*)')
+        bytes_re = re.compile(r' of (\d+) ')
+        for line in open(self.log):
+            m = line_re.match(line)
+            if not m:
+                continue
+            time = int(m.group(1))
+            verb = m.group(2)
+            if verb == 'Capturing':
+                start = time
+                captured = None
+                sent = None
+                frame_bytes = None
+            elif verb == 'Captured':
+                captured = time
+            elif verb == 'Frame':
+                m = bytes_re.match(m.group(3))
+                frame_bytes = int(m.group(1))
+            elif verb == 'Sent':
+                sent = time # TODO encode_duration
+                self.table.add(start / 1000000, frame_bytes,
+                               (captured - start) / 1000000,
+                               (sent - captured) / 1000000)
+
+        # os.unlink(self.log)
