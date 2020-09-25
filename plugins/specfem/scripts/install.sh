4281c07be37016e74803afc1e53544b0f8b5dbe3
Kevin Pouget <kpouget@redhat.com>
2020-09-25 13:29:50 +0200

    plugins/specfem/scripts/install.sh: update install script

--- plugins/specfem/scripts/install.sh
+++ plugins/specfem/scripts/install.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
-# must match plugins.specfem.measurement.SpecfemSimpleAgent.CONFIGURE_SH:
+# must match plugins.specfem.measurement.specfem_baremetal.CONFIGURE_SH:
 DATA_DIR="/data/kpouget"
 BUILD_DIR="$DATA_DIR/specfem3d_globe"
-SHARED_DIR="$DATA_DIR/shared"
+SHARED_DIR="/mnt/cephfs/kevin"
 SHARED_SPECFEM="$SHARED_DIR/specfem"
 ###
 
-dnf -y install sudo pkg-config gcc-gfortran gcc-c++ openmpi-devel openmpi cuda
+dnf -y install sudo pkg-config gcc-gfortran gcc-c++ openmpi-devel openmpi
+dnf -y cuda
 
 mkdir "$SHARED_SPECFEM"/{bin,DATABASES_MPI,OUTPUT_FILES} -p
 
@@ -19,4 +20,5 @@ cp {"$BUILD_DIR","$SHARED_SPECFEM"}/DATA -r
 
 cd "$BUILD_DIR"
 
- ./configure --enable-openmp FLAGS_CHECK=-Wno-error --with-cuda CUDA_LIB=/usr/local/cuda-11.0/targets/x86_64-linux/lib/ PATH="$PATH:/usr/lib64/openmpi/bin/"
+ ./configure --enable-openmp FLAGS_CHECK=-Wno-error \
+             --with-cuda CUDA_LIB=/usr/local/cuda-11.0/targets/x86_64-linux/lib/ PATH="$PATH:/usr/lib64/openmpi/bin/"
