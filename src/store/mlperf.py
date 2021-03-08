5c413953b0b4dffafe92ecc81af36e537ec5a112
Kevin Pouget <kpouget@redhat.com>
2021-03-08 13:31:00 +0100

    src/store/mlperf.py: start parsing mlperf data

--- src/store/mlperf.py
+++ src/store/mlperf.py
@@ -14,10 +14,18 @@ def mlperf_parse_results(dirname, import_settings):
 
     try:
         with open(f"{dirname}/stdout") as f:
-            results.word = f.read().strip()
+            for line in f.readlines():
+                if "result=" in line:
+                    results.timing = int(line.split('=')[-1].strip())
+                if "avg. samples / sec" in line:
+                    results.avg_sample_sec = float(line.split("avg. samples / sec: ")[-1].strip())
+            results.stdout = f.read().strip()
     except FileNotFoundError as e:
         print(f"{dirname}: Could not find 'stdout' file ...")
         raise e
+
+    if import_settings['cores'] == "8":
+        print("\t".join([str(import_settings[k]) for k in  sorted(import_settings)]), "\t", int(int(results.timing)/60), "min", "\t", results.avg_sample_sec, "avg. samples / sec")
     return [({}, results)]
 
 store.simple.custom_parse_results = mlperf_parse_results
