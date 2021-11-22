0a581c13cc315272c8ba5386b9bf58a2bf00f172
Kevin Pouget <kpouget@redhat.com>
2021-11-22 12:56:01 +0100

    exec/mlperf: update

--- exec/mlperf/my_run_and_time.sh
+++ exec/mlperf/my_run_and_time.sh
@@ -23,27 +23,31 @@ export NCCL_DEBUG=INFO
 
 set -e
 set -x
+set -o pipefail
+set -o nounset
 
 nvidia-smi -L
 
-NB_GPUS=$(nvidia-smi -L | grep "UUID: MIG-" | wc -l)
+NB_GPUS=$(nvidia-smi -L | grep "UUID: MIG-" | wc -l || true)
 if [[ "$NB_GPUS" == 0 ]]; then
+    # Full GPUs
     ALL_GPUS=$(nvidia-smi -L | grep "UUID: GPU" | cut -d" " -f6 | cut -d')' -f1)
     NB_GPUS=$(nvidia-smi -L | grep "UUID: GPU" | wc -l)
     MIG_MODE=0
 
-    if [[ "$GPU_RES_TYPE" != "nvidia.com/gpu" ]]; then
-        echo "FATAL: Expected full GPUs, got MIG GPUs ..."
+    if [[ "$GPU_TYPE" != "full" ]]; then
+        echo "FATAL: Expected MIG GPUs, got full GPUs ..."
         exit 1
     fi
 
     echo "No MIG GPU available, using the full GPUs ($ALL_GPUS)."
 else
+    # MIG GPUs
     ALL_GPUS=$(nvidia-smi -L | grep "UUID: MIG-" | awk '{ printf $6"\n"}' | cut -d')' -f1)
     MIG_MODE=1
 
-    if [[ "$GPU_RES_TYPE" == "nvidia.com/gpu" ]]; then
-        echo "FATAL: Expected MIG GPUs, got full GPUs ..."
+    if [[ "$GPU_TYPE" == "full" ]]; then
+        echo "FATAL: Expected full GPUs, got MIG GPUs ..."
         exit 1
     fi
 
@@ -60,7 +64,7 @@ SSD_THRESHOLD=${SSD_THRESHOLD:-0.23}
 # start timing
 start=$(date +%s)
 start_fmt=$(date +%Y-%m-%d\ %r)
-echo "STARTING TIMING RUN AT $start_fmt $GPU_MODE"
+echo "STARTING TIMING RUN AT $start_fmt $RUN_DESCR"
 
 # run benchmark
 set -x
@@ -106,6 +110,9 @@ fi
 # run training
 
 declare -a CMD
+echo "Patching 'bind_launch.py' to err-exit on failure ..."
+sed 's/process.wait()/if process.wait(): sys.exit(1)/' -i bind_launch.py
+
 CMD=('python' '-u' '-m' 'bind_launch' "--nsockets_per_node=${DGXNSOCKET}" \
                "--ncores_per_socket=${DGXSOCKETCORES}" "--nproc_per_node=${DGXNGPU}" )
 
@@ -119,23 +126,24 @@ ARGS=(train.py
   --opt-loss
   --epochs "${NUMEPOCHS}"
   --warmup-factor 0
-  #--evaluation 5 10 15 20 25 30 35 40 50 55 60 65 70 75 80 85
   --no-save
   --threshold=${SSD_THRESHOLD}
   --data ${DATASET_DIR}
   ${EXTRA_PARAMS})
 
-if [[ "$EXECUTION_MODE" == "dry" ]]; then
-    echo "Running in dry mode"
+
+if [[ "$EXECUTION_MODE" == "fast" ]]; then
+    echo "Running in FAST mode"
+    ARGS+=(--evaluation 5 10 15 20 25 30 35 40 50 55 60 65 70 75 80 85)
+elif [[ "$EXECUTION_MODE" == "dry" ]]; then
+    echo "Running in DRY mode"
     CMD[0]="echo"
 fi
 
-declare -a pids
-
 trap "date; echo failed; exit 1" ERR
 
-if [[ "$OVER_REQUESTING" != "y" ]]; then
-    SYNC_DIR=$DATASET_DIR/sync/
+if [[ "$NO_SYNC" != "y" ]]; then
+    SYNC_DIR=$DATASET_DIR/sync
 
     mkdir -p "$SYNC_DIR"
 
@@ -148,30 +156,30 @@ if [[ "$OVER_REQUESTING" != "y" ]]; then
     set +x
     echo "$(date) Waiting for all the $SYNC_COUNTER Pods to start ..."
     touch "$DATASET_DIR/sync/$SYNC_IDENTIFIER"
-    if grep $(hostname) "$DATASET_DIR/sync/$SYNC_IDENTIFIER"; then
-        echo "ERROR: $(hostname) already in the sync file ..."
-        cat "$DATASET_DIR/sync/$SYNC_IDENTIFIER"
-        exit 1
-    fi
 
-
-    echo $(hostname) >> "$DATASET_DIR/sync/$SYNC_IDENTIFIER"
     while true; do
+        if ! grep --silent $HOSTNAME "$DATASET_DIR/sync/$SYNC_IDENTIFIER"; then
+            echo "Adding $HOSTNAME to the sync file ..."
+            echo $HOSTNAME >> "$DATASET_DIR/sync/$SYNC_IDENTIFIER"
+        fi
+
         cnt=$(cat "$DATASET_DIR/sync/$SYNC_IDENTIFIER" | wc -l)
         [[ $cnt == "$SYNC_COUNTER" ]] && break
-        echo "Found $cnt Pods, waiting to have $SYNC_COUNTER ..."
+        echo "$HOSTNAME Found $cnt Pods, waiting to have $SYNC_COUNTER ..."
+        nl "$DATASET_DIR/sync/$SYNC_IDENTIFIER"
         sleep 5
     done
     echo "$(date) All the $SYNC_COUNTER Pods are running, launch the GPU workload."
-    cat "$DATASET_DIR/sync/$SYNC_IDENTIFIER"
     set -x
 else
-    echo "Over requesting mode enabled, do not wait for $SYNC_COUNTER Pods ..."
+    echo "Pod startup synchronization disabled, do not wait for $SYNC_COUNTER Pods ..."
 fi
 
 nvidia-smi -L
 
-if [[ $MIG_MODE == 1 ]]; then
+if [[ $MIG_MODE == 1 && $NB_GPUS != 1 ]]; then
+    declare -a pids
+
     for gpu in $(echo "$ALL_GPUS"); do
         export NVIDIA_VISIBLE_DEVICES=$gpu
         export CUDA_VISIBLE_DEVICES=$gpu
@@ -179,30 +187,29 @@ if [[ $MIG_MODE == 1 ]]; then
         dest=/tmp/ssd_$(echo $gpu | sed 's|/|_|g').log
 
         # run training
-        "${CMD[@]}" "${ARGS[@]}" > "$dest" &
+        "${CMD[@]}" "${ARGS[@]}" >"$dest" 2>"$dest.stderr" &
         pids+=($!)
         echo "Running on $gpu ===> $dest: PID $!"
     done
     echo "$(date): waiting for parallel $NB_GPUS executions: ${pids[@]}"
+    for pid in ${pids[@]};
+    do
+        wait $pid;
+    done
 else
     dest=/tmp/ssd_all.log
+    if [[ $MIG_MODE == 1 ]]; then
+        echo "Running on the MIG GPU"
+    else
+        echo "Running on all the $NB_GPUS GPUs "
+    fi
 
-    "${CMD[@]}" "${ARGS[@]}" > "$dest" &
-    pids+=($!)
-    echo "Running on all the GPUs ===> $dest: PID $!"
-
-    echo "$(date): waiting for 1 execution: ${pids[@]}"
-
+    "${CMD[@]}" "${ARGS[@]}" | tee -a "$dest"
 fi
 
 if [[ "$EXECUTION_MODE" == "dry" ]]; then
-    sleep 1s #2m
+    sleep 2m
 fi
-
-for pid in ${pids[@]};
-do
-    wait $pid
-done
 
 echo "$(date): done waiting for $NB_GPUS executions"
 
@@ -222,4 +229,4 @@ result=$(($end - $start))
 result_name="SINGLE_STAGE_DETECTOR"
 
 echo "RESULT,$result_name,,$result,nvidia,$start_fmt"
-echo "ALL FINISHED $GPU_MODE"
+echo "ALL FINISHED $RUN_DESCR"
--- exec/mlperf/run_ssd.py
+++ exec/mlperf/run_ssd.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/python -u
 
 import sys
 import os
@@ -24,20 +24,12 @@ kubernetes.config.load_kube_config()
 v1 = kubernetes.client.CoreV1Api()
 appsv1 = kubernetes.client.AppsV1Api()
 batchv1 = kubernetes.client.BatchV1Api()
+customv1 = kubernetes.client.CustomObjectsApi()
+
 k8s_client = kubernetes.client.ApiClient()
 
 THIS_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
-
-if not sys.stdout.isatty():
-    ARTIFACTS_DIR = Path(os.getcwd())
-else:
-    base_dir = Path("/tmp") / ("ci-artifacts_" + datetime.datetime.today().strftime("%Y%m%d"))
-    base_dir.mkdir(exist_ok=True)
-    current_length = len(list(base_dir.glob("*__*")))
-    ARTIFACTS_DIR = base_dir / f"{current_length:03d}__benchmarking__run_ssd"
-    ARTIFACTS_DIR.mkdir(exist_ok=True)
-
-print(f"Saving artifacts files into {ARTIFACTS_DIR}")
+ARTIFACTS_DIR = None # set in set_artifacts_dir
 
 NODE_NAME = "dgxa100"
 
@@ -52,44 +44,96 @@ MIG_RES_TYPES = {
 
 ###
 
+MAX_START_TIME = 5 # minutes before failing the test if some pods are still pending
+MAX_RECONFIGURE_TIME = 5 # minutes before failing the test if the MIG reconfiguration didn't complete
+
 APP_NAME = "run-ssd"
 NAMESPACE = "default"
 CONFIG_CM_NAME = "custom-config-script"
-JOB_TEMPLATE = "mlperf-ssd-job.template.yaml"
+JOB_TEMPLATE = "ssd-job.template.yaml"
 CM_FILES = [
     "my_run_and_time.sh",
 ]
 ENABLE_THANOS = False
 
 ###
+
 class objectview(object):
     def __init__(self, d):
         self.__dict__ = d
 
+def set_artifacts_dir():
+    global ARTIFACTS_DIR
+
+    if sys.stdout.isatty():
+        base_dir = Path("/tmp") / ("ci-artifacts_" + datetime.datetime.today().strftime("%Y%m%d"))
+        base_dir.mkdir(exist_ok=True)
+        current_length = len(list(base_dir.glob("*__*")))
+        ARTIFACTS_DIR = base_dir / f"{current_length:03d}__benchmarking__run_ssd"
+        ARTIFACTS_DIR.mkdir(exist_ok=True)
+    else:
+        ARTIFACTS_DIR = Path(os.getcwd())
+
+    print(f"Saving artifacts files into {ARTIFACTS_DIR}")
+
+
+def prepare_settings():
+    settings = {}
+    for arg in sys.argv[1:]:
+        k, _, v = arg.partition("=")
+        settings[k] = v
+    return settings
+
+
 def parse_gpu_settings(settings):
     ret = objectview({})
 
-    mig_mode = settings["gpu"]
+    mig_mode = settings["gpu_type"]
 
-    try:
-        mig_res_type, res_count_str, parallelism_str, *extra = mig_mode.split("_")
-        ret.res_count = int(res_count_str)
-        ret.parallelism = int(parallelism_str)
+    ret.res_count = settings["gpu_count"]
+    ret.parallelism = settings["pod_count"]
 
-        if not mig_res_type in MIG_RES_TYPES:
-            raise ValueError(f"{ret.mig_res_type} is invalid")
-    except Exception as e:
-        print(f"ERROR: failed to parse mig_mode='{mig_mode}'")
-        raise e
+    opts = []
+    if "opts" in settings:
+        opts = settings["opts"].split(",")
 
-    if mig_res_type == "full":
-        ret.k8s_res_type = "nvidia.com/gpu"
+    ret.k8s_res_types = []
+    if mig_mode not in MIG_RES_TYPES:
+        if "," not in mig_mode:
+            print(f"ERROR: failed to parse gpu_type='{mig_mode}'")
+            raise ValueError(f"{mig_mode} is invalid")
+        else:
+            for mode in mig_mode.split(","):
+                if mode not in MIG_RES_TYPES:
+                    print(f"ERROR: failed to parse gpu_type=['{mode}']")
+                    raise ValueError(f"{mode} is invalid")
+                ret.k8s_res_types.append(f"nvidia.com/mig-{mode}")
+
+    ret.mig_strategy = settings.get("mig_strategy", "mixed")
+    if ret.mig_strategy not in ("single", "mixed"):
+        raise ValueError("mig_strategy must be empty, mixed or single. Default is mixed.")
+
+    if ret.mig_strategy == "single":
+        ret.mig_label = f"all-{mig_mode}"
+
+        if ret.k8s_res_types:
+            raise ValueError("Cannot have multiple GPU types with 'single' strategy...")
+        ret.k8s_res_types.append("nvidia.com/gpu")
+
+    elif ret.k8s_res_types:
+        try:
+            ret.mig_label = settings["mig_label"]
+        except KeyError as e:
+            print("ERROR: 'mig_label' setting must be set when providing multiple 'gpu_type'")
+            raise e
+    elif mig_mode == "full":
+        ret.k8s_res_types.append("nvidia.com/gpu")
         ret.mig_label = "all-disabled"
     else:
-        ret.k8s_res_type = f"nvidia.com/mig-{mig_res_type}"
-        ret.mig_label = f"all-{mig_res_type}"
+        ret.k8s_res_types.append(f"nvidia.com/mig-{mig_mode}")
+        ret.mig_label = f"all-{mig_mode}"
 
-    return ret, extra
+    return ret, opts
 
 
 def save_thanos_metrics(thanos, thanos_start, thanos_stop):
@@ -173,7 +217,6 @@ def cleanup_pod_jobs():
         deleting_pods = []
         for pod in pods.items:
             try:
-                print("-", pod.metadata.name)
                 v1.delete_namespaced_pod(namespace=NAMESPACE, name=pod.metadata.name)
                 deleting_pods.append(pod.metadata.name)
             except kubernetes.client.exceptions.ApiException as e:
@@ -183,47 +226,62 @@ def cleanup_pod_jobs():
         time.sleep(5)
     print("Done with the Pods.")
 
-def create_job(settings, gpu_config, extra):
+SYNC_IDENTIFIER = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+def create_job(k8s_res_type, settings, gpu_config, opts):
     print(f"Running {gpu_config.parallelism} Pods in parallel")
-    print(f"Requesting {gpu_config.res_count} {gpu_config.k8s_res_type} per Pod")
+    print(f"Requesting {gpu_config.res_count} {k8s_res_type} per Pod")
 
-    over_requesting = "y" if "over" in extra else "n"
-    if over_requesting == "y":
-        print("Over requesting mode enabled.")
+    no_sync = "y" if "no-sync" in opts else "n"
+    if no_sync == "y":
+        print("Pod synchronous start disabled (no-sync)")
 
-    sync_identifier = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
 
     with open(THIS_DIR / JOB_TEMPLATE) as f:
         job_template = f.read()
 
+    run_descr = f"GPU: {settings['gpu_count']} x {settings['gpu_type']} x {settings['pod_count']} Pods"
+
+    job_name = APP_NAME
+    if len(gpu_config.k8s_res_types) > 1:
+        job_name += "-"+k8s_res_type.replace(".", "-").rpartition("/")[-1]
+
+    sync_counter = int(gpu_config.parallelism) * len(gpu_config.k8s_res_types)
+
     job_spec = job_template.format(
-        job_name=APP_NAME,
+        job_name=job_name,
+        app_name=APP_NAME,
         namespace=NAMESPACE,
 
-        k8s_res_type=gpu_config.k8s_res_type,
+        k8s_res_type=k8s_res_type,
         res_count=gpu_config.res_count,
-
         parallelism=gpu_config.parallelism,
-        over_requesting=over_requesting,
 
-        sync_identifier=sync_identifier,
+        sync_identifier=SYNC_IDENTIFIER,
+        sync_counter=sync_counter,
+        no_sync=no_sync,
 
-        settings_gpu=settings["gpu"],
+        settings_run_descr=run_descr,
         settings_cores=settings["cores"],
         settings_exec_mode=settings["execution_mode"],
         settings_threshold=settings["threshold"],
-
+        settings_gpu_type=settings["gpu_type"],
     )
 
-    print("Creating the new Job ...")
-    spec_file = ARTIFACTS_DIR / "job_spec.yaml"
+    print(f"Creating the new '{k8s_res_type}' Job ...")
+    spec_file = ARTIFACTS_DIR / f"{job_name}.job.yaml"
     with open(spec_file, "w") as out_f:
         print(job_spec, file=out_f, end="")
 
     kubernetes.utils.create_from_yaml(k8s_client, spec_file)
 
-def await_jobs():
+    print()
+    print(f"Job '{k8s_res_type}' for {run_descr} created!")
+
+def await_completion(opts):
     print("=====")
+
+    no_sync = "no-sync" in opts
 
     if ENABLE_THANOS:
         print("Thanos: Preparing  ...")
@@ -238,17 +296,22 @@ def await_jobs():
     sys.stderr.flush()
 
     started = False
-    current_phase = "..."
 
     pod_phases = defaultdict(str)
+    job_states = defaultdict(str)
+
     ERASE_LINE = "\x1b[2K\r"
+
+    wait_start = datetime.datetime.now()
+    exec_start = None
+    printed_time = None
 
     while True:
         jobs = batchv1.list_namespaced_job(namespace=NAMESPACE,
                                   label_selector=f"app={APP_NAME}")
         all_finished = True
         for job in jobs.items:
-            job = batchv1.read_namespaced_job(namespace=NAMESPACE, name=APP_NAME)
+            job = batchv1.read_namespaced_job(namespace=NAMESPACE, name=job.metadata.name)
             active = job.status.active
             succeeded = job.status.succeeded
             failed = job.status.failed
@@ -265,36 +328,99 @@ def await_jobs():
             if phase != "Finished":
                 all_finished = False
 
-            if phase != current_phase:
-                current_phase = phase
-                print("\n"+f"{job.metadata.name} - {current_phase} (active={active}, succeeded={succeeded}, failed={failed})")
+            job_state = f"{job.metadata.name} - {phase} (active={active}, succeeded={succeeded}, failed={failed})"
+            if job_state != job_states[job.metadata.name]:
+                job_states[job.metadata.name] = job_state
+                print("\n"+job_state)
+
+            if failed:
+                print(f"Failure detected in Job {job.metadata.name}, aborting...")
+
+                all_finished = True
+                break
+
+        if all_finished:
+            break
 
         pods = v1.list_namespaced_pod(namespace=NAMESPACE,
                                           label_selector=f"app={APP_NAME}")
         for pod in pods.items:
             phase = pod.status.phase
 
+            if phase == "Running":
+                if no_sync and exec_start is None:
+                    print("Execution started!")
+                    exec_start = datetime.datetime.now()
+                    printed_time = None
+                    wait_start = None
+
+                if ENABLE_THANOS and thanos_start is None:
+                    thanos_start = query_thanos.query_current_ts(thanos)
+                    print(ERASE_LINE+f"Thanos: start time: {thanos_start}")
+
             if pod_phases[pod.metadata.name] != phase:
                 print(ERASE_LINE+f"{pod.metadata.name} --> {phase}")
                 pod_phases[pod.metadata.name] = phase
 
-            if ENABLE_THANOS:
-                if thanos_start is None and phase == "Running":
-                    thanos_start = query_thanos.query_current_ts(thanos)
-                    print(ERASE_LINE+f"Thanos: start time: {thanos_start}")
+            if phase == "Failed":
+                print(f"Failure detected in Pod {pod.metadata.name}, aborting...")
+                all_finished = True
+                break
 
+        if not no_sync and exec_start is None:
+            if "Pending" not in pod_phases.values():
+                print("Execution started!")
+                exec_start = datetime.datetime.now()
+                printed_time = None
+                wait_start = None
+
+        if wait_start:
+            wait_time = (datetime.datetime.now() - wait_start).seconds / 60
+            if wait_time >= MAX_START_TIME:
+                print(ERASE_LINE+f"Pods execution didn't properly start after {wait_time:.1f} minutes, aborting...")
+                all_finished = True
+                break
+
+            if no_sync:
+                # Pods may stay pending until the previous one is finished
+                if "Running" in pod_phases.values():
+                    wait_start = None
+            else:
+                # all synced
+                if "Pending" not in pod_phases.values():
+                    wait_start = None
+
+        else:
+            if no_sync and "Running" not in pod_phases.values():
+                print("Restart waiting for Pod execution ...")
+                wait_start = datetime.datetime.now()
 
         if all_finished:
             break
 
         time.sleep(5)
-        print(".", end="")
+        if exec_start:
+            print(".", end="")
+            run_time = round((datetime.datetime.now() - exec_start).seconds / 60)
+            if not (run_time % 5) and run_time and run_time != printed_time:
+                print(ERASE_LINE + f"{run_time} minutes of execution ...")
+                printed_time = run_time
+        else:
+            print("x", end="")
+            wait_time = round((datetime.datetime.now() - wait_start).seconds / 60)
+            if not (wait_time % 1) and wait_time and wait_time != printed_time:
+                print(ERASE_LINE + f"{wait_time} minutes of wait ...")
+                printed_time = wait_time
+
         sys.stdout.flush()
 
     print("-----")
     print(datetime.datetime.now())
 
 def save_artifacts():
+    print("-----")
+    print("Collecting artifacts ...")
+
     failed = False
 
     pods = v1.list_namespaced_pod(namespace=NAMESPACE,
@@ -310,7 +436,20 @@ def save_artifacts():
         with open(dest_fname, "w") as log_f:
             print(logs, file=log_f, end="")
 
-        if phase != "Succeeded": failed = True
+        if phase != "Succeeded":
+            failed = True
+
+        if phase == "Running":
+            job_name = pod.metadata.labels["job-name"]
+            print(f"Killing Job {job_name} ...")
+            try:
+                batchv1.delete_namespaced_job(namespace=NAMESPACE, name=job_name)
+            except kubernetes.client.exceptions.ApiException as e:
+                if e.reason != "Not Found": raise e
+
+            print(f"Killing Pod {pod.metadata.name} still running ...")
+            v1.delete_namespaced_pod(namespace=NAMESPACE, name=pod.metadata.name)
+
         if not "ALL FINISHED" in logs: failed = True
         if "CUDNN_STATUS_INTERNAL_ERROR" in logs: failed = True
 
@@ -341,33 +480,121 @@ def apply_gpu_label(mig_label):
 
     v1.patch_node(NODE_NAME, body)
 
+def apply_gpu_strategy(mig_strategy):
+    print(f"Apply {mig_strategy} MIG strategy ...")
 
-def prepare_settings():
-    settings = {}
-    for arg in sys.argv[1:]:
-        k, _, v = arg.partition("=")
-        settings[k] = v
-    return settings
+    body = {
+        "spec": {
+            "mig": {
+                "strategy": mig_strategy}
+        }
+    }
 
+    customv1.patch_cluster_custom_object("nvidia.com", "v1",
+                                         "clusterpolicies", "gpu-cluster-policy",
+                                         body)
+
+def apply_gpu_label(mig_label):
+
+    node = v1.read_node(NODE_NAME)
+    if node.metadata.labels.get("nvidia.com/mig.config") == mig_label:
+        print(f"Node {NODE_NAME} already labeled with MIG label '{mig_label}', nothing to do.")
+        return
+
+    print(f"Labeling node/{NODE_NAME} with MIG label '{mig_label}' ...")
+    body = {
+        "metadata": {
+            "labels": {
+                "nvidia.com/mig.config": mig_label,
+                "nvidia.com/mig.config.state": "pending-update"
+            }
+        }
+    }
+
+    v1.patch_node(NODE_NAME, body)
+
+def wait_for_mig_reconfiguration(gpu_config):
+    print("Waiting for MIG reconfiguration of the node ...")
+    wait_start = datetime.datetime.now()
+    while True:
+        wait_time = (datetime.datetime.now() - wait_start).seconds / 60
+        if wait_time >= MAX_RECONFIGURE_TIME:
+            raise RuntimeError("MIG reconfiguration took too long ...")
+
+        sys.stdout.flush()
+        sys.stderr.flush()
+        time.sleep(5)
+        node = v1.read_node(NODE_NAME)
+
+        mig_manager_state = node.metadata.labels.get("nvidia.com/mig.config.state")
+        if mig_manager_state == "pending":
+            print("MIG Manager state:", mig_manager_state)
+            continue
+
+        if mig_manager_state == "failed":
+            raise RuntimeError("MIG reconfiguration failed ...")
+
+        print(f"MIG Manager state is {mig_manager_state}, good.")
+        if gpu_config.mig_label != "all-disabled":
+            mig_strategy = node.metadata.labels.get("nvidia.com/mig.strategy")
+            if mig_strategy != gpu_config.mig_strategy:
+                print(f"MIG strategy is wrong: {mig_strategy} ...")
+                continue
+            print(f"MIG strategy is {mig_strategy}, good.")
+
+        mig_config = node.metadata.labels.get("nvidia.com/mig.config")
+        if mig_config != gpu_config.mig_label:
+            print(f"MIG label is wrong ...")
+            continue
+        print(f"MIG label is {mig_config}, good.")
+
+        all_good = True
+        for k8s_res_type in gpu_config.k8s_res_types:
+            try:
+                if node.status.capacity[k8s_res_type] == "0":
+                    print(f"No {k8s_res_type} resources...")
+                    all_good = False
+                else:
+                    print(f"{node.status.capacity[k8s_res_type]} {k8s_res_type}, good.")
+            except KeyError:
+                # missing
+                print(f"Resource {k8s_res_type} not known...")
+                all_good = False
+
+        if not all_good:
+            continue
+
+        break
 
 def main():
     print(datetime.datetime.now())
 
+    set_artifacts_dir()
+
     settings = prepare_settings()
 
-    gpu_config, extra = parse_gpu_settings(settings)
+    gpu_config, opts = parse_gpu_settings(settings)
 
-    #
+    # --- #
 
+    apply_gpu_strategy(gpu_config.mig_strategy)
     apply_gpu_label(gpu_config.mig_label)
 
     prepare_configmap()
 
     cleanup_pod_jobs()
 
-    create_job(settings, gpu_config, extra)
+    wait_for_mig_reconfiguration(gpu_config)
 
-    await_job()
+    print(f"Launching {gpu_config.parallelism * len(gpu_config.k8s_res_types)} Pods ...")
+    for k8s_res_type in gpu_config.k8s_res_types:
+        create_job(k8s_res_type, settings, gpu_config, opts)
+
+    # --- #
+
+    await_completion(opts)
+
+    # --- #
 
     return save_artifacts()
 
