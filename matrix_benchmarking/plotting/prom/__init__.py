ec3a0bf3026dd2adc68ca609693e9ab3f5775abe
Kevin Pouget <kpouget@redhat.com>
2022-05-23 17:33:03 +0200

    plotting.prom: improve

--- matrix_benchmarking/plotting/prom/__init__.py
+++ matrix_benchmarking/plotting/prom/__init__.py
@@ -1,16 +1,27 @@
+import datetime
+
 import plotly.graph_objs as go
 
 import matrix_benchmarking.plotting.table_stats as table_stats
 import matrix_benchmarking.common as common
 
+
+def default_get_metrics(entry, metric):
+    return entry.results.metrics[metric]
+
+
 class Plot():
-    def __init__(self, podname, metric, y_title, filter_fct=lambda x:x):
-        self.podname = podname
-        self.name = f"Prom: {metric}"
+    def __init__(self, metric, y_title,
+                 get_metrics=default_get_metrics,
+                 filter_metrics=lambda x:x,
+                 as_timestamp=False):
+        self.name = f"Prom: {y_title}"
         self.id_name = f"prom_overview_{metric}"
         self.metric = metric
         self.y_title = y_title
-        self.filter_fct = filter_fct
+        self.filter_metrics = filter_metrics
+        self.get_metrics = get_metrics
+        self.as_timestamp = as_timestamp
 
         table_stats.TableStats._register_stat(self)
         common.Matrix.settings["stats"].add(self.name)
@@ -21,17 +32,22 @@ class Plot():
     def do_plot(self, ordered_vars, settings, param_lists, variables, cfg):
         fig = go.Figure()
 
-        plot_title = f"Prometheus: {self.metric}"
+        plot_title = f"Prometheus: {self.y_title}<br>({self.metric})"
         y_max = 0
         for entry in common.Matrix.all_records(settings, param_lists):
-            for metric in self.filter_fct(entry.results.metrics[self.metric]):
+            for metric in self.filter_metrics(self.get_metrics(entry, self.metric)):
                 x_values = [x for x, y in metric["values"]]
                 y_values = [float(y) for x, y in metric["values"]]
 
-                name_key = "_".join(f"{k}={settings[k]}" for k in ordered_vars)
 
-                x_start = x_values[0]
-                x_values = [x-x_start for x in x_values]
+                pod_name = metric["metric"]["pod"]
+                name_key = " ".join([pod_name, "_".join(f"{k}={settings[k]}" for k in ordered_vars)])
+
+                if self.as_timestamp:
+                    x_values = [datetime.datetime.fromtimestamp(x) for x in x_values]
+                else:
+                    x_start = x_values[0]
+                    x_values = [x-x_start for x in x_values]
 
                 y_max = max([y_max]+y_values)
 
