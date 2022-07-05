986967803d942020f2a3bf72fc89f8df9149d97c
Kevin Pouget <kpouget@redhat.com>
2022-07-05 15:33:09 +0200

    matrix_benchmarking/plotting/prom/__init__.py: plot multiple metrics at the same time

--- matrix_benchmarking/plotting/prom/__init__.py
+++ matrix_benchmarking/plotting/prom/__init__.py
@@ -11,13 +11,14 @@ def default_get_metrics(entry, metric):
 
 
 class Plot():
-    def __init__(self, metric, y_title,
+    def __init__(self, metrics, y_title,
                  get_metrics=default_get_metrics,
                  filter_metrics=lambda x:x,
                  as_timestamp=False):
         self.name = f"Prom: {y_title}"
-        self.id_name = f"prom_overview_{metric}"
-        self.metric = metric
+
+        self.id_name = f"prom_overview_{y_title}"
+        self.metrics = metrics
         self.y_title = y_title
         self.filter_metrics = filter_metrics
         self.get_metrics = get_metrics
@@ -32,31 +33,30 @@ class Plot():
     def do_plot(self, ordered_vars, settings, param_lists, variables, cfg):
         fig = go.Figure()
 
-        plot_title = f"Prometheus: {self.y_title}<br>({self.metric})"
+        plot_title = f"Prometheus: {self.y_title}<br>({'<br>'.join(self.metrics)})"
         y_max = 0
         for entry in common.Matrix.all_records(settings, param_lists):
-            for metric in self.filter_metrics(self.get_metrics(entry, self.metric)):
-                x_values = [x for x, y in metric["values"]]
-                y_values = [float(y) for x, y in metric["values"]]
+            for metric_name in self.metrics:
+                for metric in self.filter_metrics(self.get_metrics(entry, metric_name)):
+                    x_values = [x for x, y in metric["values"]]
+                    y_values = [float(y) for x, y in metric["values"]]
 
+                    legend_name = metric_name #.partition("{")[0]
 
-                pod_name = metric["metric"]["pod"]
-                name_key = " ".join([pod_name, "_".join(f"{k}={settings[k]}" for k in ordered_vars)])
+                    if self.as_timestamp:
+                        x_values = [datetime.datetime.fromtimestamp(x) for x in x_values]
+                    else:
+                        x_start = x_values[0]
+                        x_values = [x-x_start for x in x_values]
 
-                if self.as_timestamp:
-                    x_values = [datetime.datetime.fromtimestamp(x) for x in x_values]
-                else:
-                    x_start = x_values[0]
-                    x_values = [x-x_start for x in x_values]
+                    y_max = max([y_max]+y_values)
 
-                y_max = max([y_max]+y_values)
-
-                trace = go.Scatter(x=x_values, y=y_values,
-                                   name=name_key,
+                    trace = go.Scatter(x=x_values, y=y_values,
+                                   name=legend_name,
                                    hoverlabel= {'namelength' :-1},
                                    showlegend=True,
                                    mode='markers+lines')
-                fig.add_trace(trace)
+                    fig.add_trace(trace)
 
         fig.update_layout(
             title=plot_title, title_x=0.5,
