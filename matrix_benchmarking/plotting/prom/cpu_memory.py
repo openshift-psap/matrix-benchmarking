3962d33ea9cf430be14344d21f19db7c7fa5bc9a
Keith Valin <kvalin@redhat.com>
2023-04-25 15:01:47 -0400

    Testing fix for RHODS CPU Usage graph

--- matrix_benchmarking/plotting/prom/cpu_memory.py
+++ matrix_benchmarking/plotting/prom/cpu_memory.py
@@ -46,6 +46,197 @@ class Plot():
     def do_hover(self, meta_value, variables, figure, data, click_info):
         return "nothing"
 
+    def do_lts_plot(self, ordered_vars, settings, setting_lists, variables, cfg):
+        cfg__check_all_thresholds = cfg.get("check_all_thresholds", False)
+
+        fig = go.Figure()
+        metric_names = [
+            list(metric.items())[0][0] if isinstance(metric, dict) else metric
+            for metric in self.metrics
+        ]
+
+        plot_title = f"Prometheus: {self.y_title}"
+        y_max = 0
+
+        y_divisor = 1024*1024*1024 if self.is_memory else 1
+
+        single_expe = sum(1 for _ in common.Matrix.all_lts_records(settings, setting_lists)) == 1
+
+        data = []
+        data_rq = []
+        data_lm = []
+        data_threshold = []
+
+        threshold_status = defaultdict(list)
+        threshold_passes = defaultdict(int)
+        for entry in common.Matrix.all_lts_records(settings, setting_lists):
+            try: threshold_value = entry.data['thresholds'].get(self.threshold_key) if self.threshold_key else None
+            except AttributeError: threshold_value = None
+            check_thresholds = True
+
+            if cfg__check_all_thresholds:
+                check_thresholds = True
+
+            entry_name = ", ".join([f"{key}={entry.metadata['settings'][key]}" for key in variables])
+
+            sort_index = entry.metadata['settings'][ordered_vars[0]] if len(variables) == 1 \
+                else entry_name
+
+            for _metric in self.metrics:
+                metric_name, metric_query = list(_metric.items())[0] if isinstance(_metric, dict) else (_metric, _metric)
+
+                for metric in self.filter_metrics(entry, self.get_metrics(entry, metric_name)):
+                    if not metric: continue
+
+                    x_values = [x for x, y in metric["values"]]
+                    y_values = [float(y)/y_divisor for x, y in metric["values"]]
+
+                    metric_actual_name = metric["metric"].get("__name__", metric_name)
+                    legend_name = metric_actual_name
+                    if metric["metric"].get("container") == "POD": continue
+
+                    if "_sum_" in metric_name:
+                        legend_group = None
+                        legend_name = "sum(all)"
+                    else:
+                        legend_group = metric["metric"].get("pod", "<no podname>") + "/" + metric["metric"].get("container", self.container_name) \
+                            if not self.is_cluster else None
+
+                    if self.as_timestamp:
+                        x_values = [datetime.datetime.fromtimestamp(x) for x in x_values]
+                    else:
+                        x_start = x_values[0]
+                        x_values = [x-x_start for x in x_values]
+
+                    y_max = max([y_max]+y_values)
+
+                    opts = {}
+
+                    if self.skip_nodes and "node" not in metric["metric"]:
+                        continue
+
+                    is_req_or_lim = "limit" in legend_name or "requests" in legend_name
+
+                    if single_expe:
+                        entry_name = "Test"
+
+                        if "requests" in metric_actual_name:
+                            opts["line_color"] = "orange"
+                            opts["line_dash"] = "dot"
+                            opts["mode"] = "lines"
+
+                        elif "limits" in metric_actual_name or "capacity" in metric_actual_name:
+                            opts["line_color"] = "red"
+                            opts["mode"] = "lines"
+                            opts["line_dash"] = "dash"
+                        else:
+                            opts["mode"] = "markers+lines"
+
+                        data.append(
+                            go.Scatter(x=x_values, y=y_values,
+                                       name=legend_name,
+                                       hoverlabel= {'namelength' :-1},
+                                       showlegend=True,
+                                       legendgroup=legend_group,
+                                       legendgrouptitle_text=legend_group,
+                                       **opts))
+
+                        if not is_req_or_lim and check_thresholds:
+                            data.append(
+                                go.Scatter(x=[x_values[0], x_values[-1]], y=[threshold_value, threshold_value],
+                                           name="threshold",
+                                           hoverlabel= {'namelength' :-1},
+                                           showlegend=True,
+                                           line_color="red",
+                                           marker=dict(color='red', size=15, symbol="triangle-down"),
+                                           legendgroup=legend_group,
+                                           legendgrouptitle_text=legend_group))
+
+                    else:
+                        if threshold_value:
+                            data_threshold.append(dict(Version=entry_name,
+                                                       SortIndex=sort_index,
+                                                       Value=threshold_value,
+                                                       Metric=legend_name))
+
+                        if is_req_or_lim:
+                            lst = data_lm if "limits" in legend_name else data_rq
+
+                            lst.append(dict(Version=entry_name,
+                                            SortIndex=sort_index,
+                                            Value=y_values[0],
+                                            Metric=legend_name))
+
+                        else:
+                            for y_value in y_values:
+                                data.append(dict(Version=entry_name,
+                                                 SortIndex=sort_index,
+                                                 Metric=legend_name,
+                                                 Value=y_value))
+
+
+                    if not is_req_or_lim and threshold_value and check_thresholds:
+
+                        if max(y_values) > float(threshold_value):
+                            status = f"FAIL: {max(y_values):.2f} > threshold={threshold_value}"
+                        else:
+                            status = f"PASS: {max(y_values):.2f} <= threshold={threshold_value}"
+                            threshold_passes[entry_name] += 1
+
+                        threshold_status[entry_name].append(status)
+
+        if not data:
+            return None, "No data to plot ..."
+
+        if single_expe:
+            fig = go.Figure(data=data)
+
+            fig.update_layout(
+                title=plot_title, title_x=0.5,
+                yaxis=dict(title=self.y_title + (" (in Gi)" if self.is_memory else ""), range=[0, y_max*1.05]),
+                xaxis=dict(title=f"Time (in s)"))
+        else:
+            df = pd.DataFrame(data).sort_values(by=["SortIndex"])
+
+            fig = px.box(df, x="Version", y="Value", color="Version")
+            fig.update_layout(
+                title=plot_title, title_x=0.5,
+                yaxis=dict(title=self.y_title + (" (in Gi)" if self.is_memory else ""))
+            )
+            if data_rq:
+                df_rq = pd.DataFrame(data_rq).sort_values(by=["SortIndex"])
+                fig.add_scatter(name="Request",
+                                x=df_rq['Version'], y=df_rq['Value'], mode='lines',
+                                line=dict(color='orange', width=5, dash='dot'))
+            if data_lm:
+                df_lm = pd.DataFrame(data_lm).sort_values(by=["SortIndex"])
+                fig.add_scatter(name="Limit",
+                                x=df_lm['Version'], y=df_lm['Value'], mode='lines',
+                                line=dict(color='red', width=5, dash='dot'))
+            if data_threshold:
+                df_threshold = pd.DataFrame(data_threshold).sort_values(by=["SortIndex"])
+                fig.add_scatter(name="Threshold",
+                                x=df_threshold['Version'], y=df_threshold['Value'], mode='lines+markers',
+                                marker=dict(color='red', size=15, symbol="triangle-down"),
+                                line=dict(color='brown', width=5, dash='dot'))
+
+        msg = []
+        if threshold_status:
+            msg.append(html.H3(self.y_title))
+
+        for entry_name, status in threshold_status.items():
+            total_count = len(status)
+            pass_count = threshold_passes[entry_name]
+            success = pass_count == total_count
+            msg += [html.B(entry_name), ": ", html.B("PASSED" if success else "FAILED"), f" ({pass_count}/{total_count} success{'es' if pass_count > 1 else ''})"]
+            details = []
+            for a_status in status:
+                details.append(html.Li(a_status))
+
+            msg.append(html.Ul(details))
+
+        return fig, msg
+
     def do_plot(self, ordered_vars, settings, setting_lists, variables, cfg):
         cfg__check_all_thresholds = cfg.get("check_all_thresholds", False)
 
@@ -61,6 +252,8 @@ class Plot():
         y_divisor = 1024*1024*1024 if self.is_memory else 1
 
         single_expe = sum(1 for _ in common.Matrix.all_records(settings, setting_lists)) == 1
+        if sum(1 for _ in common.Matrix.all_lts_records(settings, setting_lists)) >= 1:
+            return self.do_lts_plot(ordered_vars, settings, setting_lists, variables, cfg)
 
         data = []
         data_rq = []
