b212189cde3f130eeb4a12d1247d4d3bea9330df
Keith Valin <kvalin@redhat.com>
2023-05-25 13:22:48 -0400

    common: Add member functions for MatrixEntry, added LTS payload support, added type hints

--- matrix_benchmarking/common.py
+++ matrix_benchmarking/common.py
@@ -1,33 +1,51 @@
+from typing import Iterator
+
 import os, types, itertools
 from collections import defaultdict
 import pathlib
 
 import matrix_benchmarking
 
+
 class MatrixEntry(types.SimpleNamespace):
     def __init__(self, location, results,
                  processed_key, import_key,
                  processed_settings, import_settings, settings=None,
-                 stats=None, is_gathered=None):
-        if settings:
-            self.settings = settings
-        else:
-            self.settings = types.SimpleNamespace()
+                 stats=None, is_gathered=None, is_lts=False):
+        self.is_lts = is_lts
+        self.is_gathered = False
+
+        self.settings = settings or types.SimpleNamespace()
+
         self.stats = {}
 
         self.location = location
         self.results = results
 
         self.settings.__dict__.update(processed_settings)
+
         self.processed_key = processed_key
         self.import_settings = processed_settings
-
-        self.is_gathered = False
 
         Matrix.import_map[import_key] = \
         Matrix.processed_map[processed_key] = self
 
         [Matrix.settings[k].add(v) for k, v in processed_settings.items()]
+
+    def get_name(self, variables) -> str:
+        return ", ".join([f"{key}={self.settings.__dict__[key]}" for key in variables])
+
+    def get_threshold(self, threshold_value, default: str = None) -> str:
+        if hasattr(self.results, 'thresholds'):
+            return self.results.thresholds.get(threshold_value, default)
+        return default
+    
+    def get_settings(self) -> dict:
+        return self.settings.__dict__
+    
+    def check_thresholds(self) -> bool:
+        return hasattr(self.results, 'check_thresholds') and self.results.check_thresholds
+
 
 
 class Matrix():
@@ -40,14 +58,23 @@ class Matrix():
         return "|".join(f"{k}={settings[k]}" for k in sorted(settings) if k != "stats")
 
     @staticmethod
-    def all_records(settings, setting_lists):
+    def all_records(settings, setting_lists, include_local=True, include_lts=False, local_first=True) -> Iterator[MatrixEntry]:
+        if local_first: # Does nothing if both are not set to true, this is to provide a default behaviour
+            if include_local and include_lts:
+                yield from Matrix.all_records(settings, setting_lists, include_local=True, include_lts=False)
+                yield from Matrix.all_records(settings, setting_lists, include_local=False, include_lts=True)
+                return
+
         for settings_values in sorted(itertools.product(*setting_lists)):
             settings.update(dict(settings_values))
             key = Matrix.settings_to_key(settings)
+
             try:
-                yield Matrix.processed_map[key]
+                entry = Matrix.processed_map[key]
             except KeyError:
                 continue # missing experiment
+            if (entry.is_lts and include_lts) or (not entry.is_lts and include_local):
+                yield entry
 
     @staticmethod
     def get_record(settings):
@@ -55,3 +82,13 @@ class Matrix():
 
         try: return Matrix.processed_map[key]
         except KeyError: return None
+
+    @staticmethod
+    def count_records(settings, setting_lists, include_local=True, include_lts=False):
+        return sum([ 1 for _ in Matrix.all_records(settings, setting_lists, include_local, include_lts) ])
+
+    @staticmethod
+    def has_records(settings, setting_lists, include_local=True, include_lts=False):
+         for _ in Matrix.all_records(settings, setting_lists, include_local, include_lts):
+              return True
+         return False
