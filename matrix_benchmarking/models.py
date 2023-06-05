ebb90ffbe704e69b2276881be85a929c2df64850
Keith Valin <kvalin@redhat.com>
2023-06-05 13:41:26 -0400

    Added ExclusiveModel to prevent additional properties

--- matrix_benchmarking/models.py
+++ matrix_benchmarking/models.py
@@ -2,35 +2,39 @@ from typing import List, Tuple
 import datetime as dt
 from enum import Enum
 
-from pydantic import BaseModel, ConstrainedStr, constr
+from pydantic import BaseModel, ConstrainedStr, constr, Extra
 
 SEMVER_REGEX="(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?"
 
-class Metadata(BaseModel):
+class ExclusiveModel(BaseModel):
+    class Config:
+        extra = Extra.forbid
+
+class Metadata(ExclusiveModel):
     start: dt.datetime
     end: dt.datetime
 
 
 def create_PSAPPayload(schema_name):
-    class PSAPPayload(BaseModel):
+    class PSAPPayload(ExclusiveModel):
         """
         The underlying model of PSAP payloads
         """
         payload_schema: constr(regex=f"^urn:{schema_name}:{SEMVER_REGEX}$")
         data: dict
         metadata: Metadata
-        
+
         class Config:
             fields = {'payload_schema': '$schema'}
     return PSAPPayload
 
 
-class PrometheusValue(BaseModel):
+class PrometheusValue(ExclusiveModel):
     metric: dict
     values: List[Tuple[int, str]]
 
 
-class PrometheusMetric(BaseModel):
+class PrometheusMetric(ExclusiveModel):
     query: str
     data: List[PrometheusValue]
 
