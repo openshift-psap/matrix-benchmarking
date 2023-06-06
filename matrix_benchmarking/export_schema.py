3b0b3359194131e0169c40a991b7fb9b0a2ee07f
Keith Valin <kvalin@redhat.com>
2023-06-06 14:40:51 -0400

    Updated export_schema to output to a file

--- matrix_benchmarking/export_schema.py
+++ matrix_benchmarking/export_schema.py
@@ -3,12 +3,13 @@ import getpass
 import datetime as dt
 import requests
 import json
+import sys
 
 import matrix_benchmarking.common as common
 import matrix_benchmarking.cli_args as cli_args
 import matrix_benchmarking.store as store
 
-def main(workload: str = ""):
+def main(workload: str = "", file: str = "-"):
     """
 Export a JSON Schema for a particular workload
 
@@ -16,6 +17,7 @@ Export a JSON Schema for a workload, which is commonly used in Horreum
 
 Args:
     workload: Name of the workload to execute. (Mandatory)
+    file: Name of the file to export the schema to, "-" to output to stdout.  (Default is "-")
     """
     kwargs = dict(locals())
 
@@ -26,6 +28,12 @@ Args:
         cli_args.store_kwargs(kwargs, execution_mode="export_schema")
         workload_store = store.load_workload_store(kwargs)
 
-        print(store._get_custom_schema().schema_json(indent=4))
+        if file == '-':
+            output = sys.stdout
+        else:
+            output = open(file, 'w')
+
+        output.write(store.get_custom_schema().schema_json(indent=4))
+        output.close()
    
     return cli_args.TaskRunner(run)
