a8d48fdddd8d4c912809fbb2bd4a29c001bbb9b7
Keith Valin <kvalin@redhat.com>
2023-06-30 19:36:01 -0400

    Update to use v1 module within pydantic v2

--- matrix_benchmarking/models.py
+++ matrix_benchmarking/models.py
@@ -4,7 +4,7 @@ from typing import List, Tuple, Dict, Union, Optional
 import datetime as dt
 from enum import Enum
 
-from pydantic import BaseModel, ConstrainedStr, constr, Extra
+from pydantic.v1 import BaseModel, ConstrainedStr, constr, Extra
 import pydantic
 
 SEMVER_REGEX="(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?"
@@ -15,7 +15,7 @@ class ExclusiveModel(BaseModel):
     class Config:
         extra = Extra.forbid
 
-class AllOptional(pydantic.main.ModelMetaclass):
+class AllOptional(pydantic.v1.main.ModelMetaclass):
     def __new__(self, name, bases, namespaces, **kwargs):
         annotations = namespaces.get('__annotations__', {})
         for base in bases:
